0bc04ce82b387561f3ffcde64f43537439897923
starrylife <forinnocen@naver.com>
2024-03-21 19:32:22 +0900

    0321

--- pages/3 안건 제안 보조 챗봇.py
+++ pages/3 안건 제안 보조 챗봇.py
@@ -19,7 +19,7 @@ if password == correct_password:
         if "thread_id" not in st.session_state:
             st.session_state.thread_id = ""
 
-        thread_btn = st.button("Create a new thread")
+        thread_btn = st.button("Thread 생성")
 
         if thread_btn:
             thread = client.beta.threads.create()
@@ -39,7 +39,7 @@ if password == correct_password:
 
     st.title("안건 제안 보조 챗봇")
     if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "assistant", "content": "안녕하세요, 저는 안건 제안 보조 챗봇입니다. 먼저 왼쪽의 Thread 생성버튼을 눌러주세요. 무엇을 도와드릴까요?"}]
+        st.session_state["messages"] = [{"role": "assistant", "content": "안녕하세요, 저는 안건 제안 보조 챗봇입니다. 먼저 왼쪽의 'Thread 생성'버튼을 눌러주세요. 무엇을 도와드릴까요?"}]
 
     for msg in st.session_state.messages:
         st.chat_message(msg["role"]).write(msg["content"])
